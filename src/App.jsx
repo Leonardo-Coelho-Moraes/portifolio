8bf3805604fae7ce496c9a2f41250bdbf21e31a5
Leonardo Coelho Moraes <108236132+Leonardo-Coelho-Moraes@users.noreply.github.com>
2023-04-04 17:48:27 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Button from './components/Button';
 import { FiGithub, FiLinkedin, FiDownloadCloud, FiMail} from "react-icons/fi";
 import conhecimentos from './conhecimentos';
 import Projeto from './components/Projeto';
-import link from 'https://drive.google.com/file/d/1YAUF1VdEfP0aDPu3_GkFEP0jRxGkEXsQ/view?usp=sharing'
+
 
 function App() {
   const [descriAtual, setDescriAtual] = useState('HTML5 é uma linguagem de marcação para a World Wide Web e é uma tecnologia chave da Internet, originalmente proposto por Opera Software. É a quinta versão da linguagem HTML.');
@@ -31,7 +31,7 @@ function App() {
               <div className='container sec1conteudo flex'>
                   <h2>Olá, eu sou <br/> <span>leonardo coelho</span> <br/> Desenvolvedor Front-end  </h2>
                   <div className='btns flex'>
-                  <Button caminho={link}  cor1='#9358F7' cor2='#10D7E2' name='Linkedin' icon={FiLinkedin}  />
+                  <Button   cor1='#9358F7' cor2='#10D7E2' name='Linkedin' icon={FiLinkedin}  />
                   <Button cor1='#D24074' cor2='#6518B4' name='Github' icon={FiGithub} />
                   </div>
               </div>
@@ -44,7 +44,7 @@ function App() {
                     <h4>Coari-AM, Brasil</h4>
                     <p>Meu nome é Leonardo e desde muito novo gosto de computadores. Minha meta é oferecer um serviço o mais completo possível, desde o trabalho com imagens até o trabalho com vídeos. Meu foco principal é em desenvolvimento para a web :)</p>
                     <div className='btns flex'>
-                    <Button cor1='#D24074' cor2='#6518B4' name='Currículo' icon={FiDownloadCloud} />
+                    <Button caminho='https://drive.google.com/file/d/1YAUF1VdEfP0aDPu3_GkFEP0jRxGkEXsQ/view?usp=sharing'cor1='#D24074' cor2='#6518B4' name='Currículo' icon={FiDownloadCloud} />
                   <Button   cor1='#9358F7' cor2='#10D7E2' name='E-mail' icon={FiMail} />
                  
                   </div>
